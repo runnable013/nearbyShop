90e509f6ef5d230b917020f611eb3de39dac1ef0
Runnable <42142686+runnable013@users.noreply.github.com>
2018-11-15 17:24:49 +0900

    애니메이션 삭제

--- js/.js
+++ js/.js
@@ -5,14 +5,3 @@ window.onload = function() {
     logo = document.getElementById("logo");
     search = document.getElementById("search");
 }
-
-function disSearch() {
-    setTimeout(function() {
-        logo.style.display = "none";
-        search.style.display = "flex";
-    }, 700);
-}
-
-function onSearch() {
-
-}
