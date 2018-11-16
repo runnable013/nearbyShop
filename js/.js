1d87f696520c4571bdc6525979b9936c973ab0f7
Runnable <42142686+runnable013@users.noreply.github.com>
2018-11-16 16:03:42 +0900

    검색페이지로 이동

--- js/.js
+++ js/.js
@@ -5,3 +5,7 @@ window.onload = function() {
     logo = document.getElementById("logo");
     search = document.getElementById("search");
 }
+
+function onSearch() {
+    location.href="C:/Users/qqaa2/Documents/GitHub/nearbyShop/search.html";
+}
